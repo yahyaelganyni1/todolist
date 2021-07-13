bb8c2c676f17e4fdd2d6ed1e152e1edbd3fef4b2
yahyaelganyni1 <crazy20046@gmail.com>
2021-07-14 01:44:40 +0200

    add logic style & html

--- SRC/index.js
+++ SRC/index.js
@@ -1,15 +1,45 @@
 import _ from 'lodash';
 import './style.css';
+const theList = document.querySelector('.the-list');
+const inpotBox = document.querySelector('.ibput-box');
+const listItem = [
+  {
+    description: 'do stuff 1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'do stuff 2',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'do stuff 3',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'do stuff 4',
+    completed: false,
+    index: 3,
+  },
+];
 
-// function component() {
-//   const element = document.createElement('div');
+// creat list element
 
-//   // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const listElemenet = (description, completed, index) => {
+  const li = document.createElement('li');
+  const text = document.createElement('p');
+  text.innerHTML = description;
+  const checbox = document.createElement('input');
+  checbox.setAttribute('type', 'checkbox');
+  li.appendChild(checbox);
+  li.appendChild(text);
+  theList.appendChild(li);
+};
 
-//   return element;
-// }
+listItem.forEach((item) => {
+  listElemenet(item.description);
+});
 
-// document.body.appendChild(component());
-
-console.log('object');
+console.log(listItem[0]);
